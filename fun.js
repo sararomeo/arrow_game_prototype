3d9b4998d44b08b57d028b0c9ade0998e1c2544a
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-11-12 19:02:27 +0100

    Fix shift pos

--- fun.js
+++ fun.js
@@ -6,6 +6,30 @@ document.addEventListener("DOMContentLoaded", function () {
     const visibility_states = option_icon_ids.map(() => false);
     const baseShift = {};
     const originalPositions = {};
+    const arrowshaftOccupied = [false, false, false];
+    const occupyArrowShaft = (index, id) => {
+        if (!arrowshaftOccupied[index]) {
+            arrowshaftOccupied[index] = id;
+            console.log(`Arrow shaft ${index} is now occupied by ${id}.`);
+        } else {
+            console.log(`Arrow shaft ${index} is already occupied by ${arrowshaftOccupied[index]}.`);
+        }
+        console.log("ARROWSHAFTS", arrowshaftOccupied);
+    }
+    const getArrowShaftIndex = (id) => {
+        return arrowshaftOccupied.findIndex((occupiedId) => occupiedId === id);
+    };
+    const freeArrowShaft = (id) => {
+        const index = arrowshaftOccupied.indexOf(id);
+        if (index !== -1) {
+            arrowshaftOccupied[index] = false;
+            console.log(`Arrow shaft ${index} is now free.`);
+        } else {
+            console.log(`Arrow shaft associated with ${id} is not occupied.`);
+        }
+        console.log("ARROWSHAFTS", arrowshaftOccupied);
+        return index;
+    }
 
     // Wait for the A-Frame scene to load and then get original position and init shift from origin for each 3D model.
     const scene = document.querySelector('a-scene');
@@ -19,6 +43,44 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
+    const shiftPos = (entity, id, arrowshaft_num, visible) => {
+        if (originalPositions[id]) {
+            console.log("VISIBLE: ", visible);
+            if (visible) {
+                console.log("ORIGINAL POS: ", originalPositions[id])
+
+                const shift = baseShift[id] * arrowshaft_num;
+                console.log("shift: ", shift);
+
+                newX = originalPositions[id].x + shift;
+                console.log("ORIGINAL POS (" + originalPositions[id].x + ") + shift (" + shift + ") = " + newX);
+
+                entity.setAttribute("position", {
+                    x: newX,
+                    y: originalPositions[id].y,
+                    z: originalPositions[id].z
+                });
+                console.log("Final x pos: ", newX);
+            } else if (!visible) {
+                console.log("ORIGINAL POS: ", originalPositions[id])
+
+                const shift = baseShift[id] * arrowshaft_num;
+                console.log("shift: ", shift);
+
+                newX = originalPositions[id].x - shift;
+                console.log("ORIGINAL POS (" + originalPositions[id].x + ") - shift (" + shift + ") = " + newX);
+
+                entity.setAttribute("position", {
+                    x: newX,
+                    y: originalPositions[id].y,
+                    z: originalPositions[id].z
+                });
+                console.log("Final x pos: ", newX);
+            }
+        }
+        entity.setAttribute("visible", visible);
+    }
+
     /**
      *  Set the visibility of a 3D model shifting it to the correct position and update the menu icon style.
      * @param {*} button 
@@ -28,47 +90,64 @@ document.addEventListener("DOMContentLoaded", function () {
      */
     const setVisible = (button, entities, visible, id) => {
         const num_visibles = visibility_states.filter(state => state == true).length - 1;
+        flag = false;
+
+        console.log("\n\n NUM VIS: ", num_visibles);
+
         entities.forEach(entity => {
-
-            if (originalPositions[id]) {
-                const shift = baseShift[id] * num_visibles;
-                
-                console.log("\n\nNUM VIS: ", num_visibles);
-                console.log("SHIFT: ", shift);
-                console.log("VISIBLE: ", visible);
-                console.log("original pos: ", originalPositions[id]);
-
-                if (visible) {
-                    entity.setAttribute("position", {
-                        x: originalPositions[id].x + shift,
-                        y: originalPositions[id].y,
-                        z: originalPositions[id].z
-                    });
-                    console.log("X shift: ", originalPositions[id].x + shift);
-                } else {
-                    entity.setAttribute("position", {
-                        x: originalPositions[id].x - shift,
-                        y: originalPositions[id].y,
-                        z: originalPositions[id].z
-                    });
-                    console.log("X shift: ", originalPositions[id].x - shift);
-                }
+            if (visible) {
+                switch (num_visibles) {
+                    case 0:
+                        console.log('case 0', num_visibles);
+                        arrowshaft_num = 0;
+                        occupyArrowShaft(arrowshaft_num, id);
+                        shiftPos(entity, id, arrowshaft_num, visible);
+                        break;
+                    case 1:
+                        console.log('case 1', num_visibles);
+                        if (arrowshaftOccupied[0] != false) {
+                            arrowshaft_num = 1;
+                        } else {
+                            arrowshaft_num = 0;
+                        }
+                        occupyArrowShaft(arrowshaft_num, id);
+                        shiftPos(entity, id, arrowshaft_num, visible);
+                        break;
+                    case 2:
+                        console.log('case 2', num_visibles);
+                        if (arrowshaftOccupied[0] != false && arrowshaftOccupied[1] != false) {
+                            arrowshaft_num = 2;
+                        } else if (arrowshaftOccupied[0] != false && arrowshaftOccupied[2] != false) {
+                            arrowshaft_num = 1;
+                        } else if (arrowshaftOccupied[1] != false && arrowshaftOccupied[2] != false) {
+                            arrowshaft_num = 0;
+                        }
+                        occupyArrowShaft(arrowshaft_num, id);                        
+                        shiftPos(entity, id, arrowshaft_num, visible);
+                        break;
+                    default:
+                        flag = true;
+                        console.log(`do nothin bc >=3`);
+                        break;
+                    }
+            } else {
+                indexx = getArrowShaftIndex(id);
+                freeArrowShaft(id);
+                console.log("REMOVE FROM and baseshift*arrowshaft: ", indexx);
+                shiftPos(entity, id, indexx, visible); // put into original pos
             }
-            entity.setAttribute("visible", visible);
-
-
         });
-        button.classList.toggle("selected", visible);
-
+        if (!flag) {
+            button.classList.toggle("selected", visible);
+        }
     };
 
-
+    // Set initial visibility and add a click event listener to toggle visibility of the 3D model.
     option_icon_ids.forEach((id, index) => {
         const button = document.querySelector(`#${id}`);
         const entities = document.querySelectorAll(`.${id}-entity`);
-        // Set initial visibility.
-        setVisible(button, entities, visibility_states[index]);
-        // Add a click event listener to toggle visibility of the 3D model on click
+        setVisible(button, entities, visibility_states[index], id);
+        
         button.addEventListener('click', () => {
             visibility_states[index] = !visibility_states[index];
             setVisible(button, entities, visibility_states[index], id);
